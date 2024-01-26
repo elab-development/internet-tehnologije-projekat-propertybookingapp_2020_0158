146f679301fdf1976c0fb915f75ce0412631c8b6
Marta Magovcevic <mm20200158@student.fon.bg.ac.rs>
2024-01-26 03:08:00 +0100

    Kreirana welcome stranica i CSS za nju

---
+++ propertybookingfront/src/components/Welcome/WelcomePage.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './WelcomePage.css';
+
+const WelcomePage = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await axios.get('https://restcountries.com/v3.1/all');
+        setCountries(response.data.slice(0, 15)); // Uzimamo samo prvih 15
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
+  return (
+    <div className="welcome-page">
+      <div className='welcome-text'>
+      <h1>Dobrodošli na Property Booking</h1>
+      <p>
+        Property Booking je platforma koja vam omogućava da pronađete savršen smještaj za vaše putovanje. Bilo da tražite luksuzni hotel, udoban apartman ili rustičnu kućicu, mi imamo sve!
+      </p>
+      <h2>Informacije o zemljama za koje nudimo rezervisanje nekretnina:</h2>
+      </div>
+      <div className="country-list">
+        <ul>
+          {countries.map((country, index) => (
+            <li key={index}>
+              <h3>{country.name.common}</h3>
+              <p><strong>Glavni grad:</strong> {country.capital}</p>
+              <img src={country.flags.svg} alt={`Zastava ${country.name.common}`} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
