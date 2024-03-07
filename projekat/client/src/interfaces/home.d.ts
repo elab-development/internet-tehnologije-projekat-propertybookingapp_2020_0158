e0c541d52ccf318c9c9a681038d5c5fdbc2c5478
Marta Magovcevic <mm20200158@student.fon.bg.ac.rs>
2024-03-07 11:00:20 +0100

    Kreirani Typescript interfejsi

---
+++ projekat/client/src/interfaces/home.d.ts
@@ -0,0 +1,12 @@
+export interface PieChartProps {
+    title: string,
+    value: number,
+    series: Array<number>,
+    colors: Array<string>,
+    numOfMyProps: number
+}
+
+export interface SatisfactionBoxProps{
+    title: string,
+    value: number
+}
